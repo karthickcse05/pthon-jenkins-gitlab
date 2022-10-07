11cfd1d5d27b38e774498e445395ff7fa452a9ec
Karthick <40426955+karthickcse05@users.noreply.github.com>
2022-10-07 16:26:24 +0530

    Update test-gitlab.py

--- test-gitlab.py
+++ test-gitlab.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     id = os.getenv("Project_ID")
     print(token)
     print(id)
-    response = requests.get("https://gitlab.tis.trane.com/api/v4/projects/" + id + "/pipeline_schedules",
+    response = requests.get("https://gitlab.com/api/v4/projects/" + id + "/pipeline_schedules",
                             headers={"PRIVATE-TOKEN": token})
     if (response.status_code == 200):
         l_PipelineList = json.loads(response.content)
